5cdad6b7a5b170bb2ceb61d588273174cd147951
edwin-droid <edwinfgt15@gmail.com>
2026-06-05 21:17:51 -0500

    in progress

---
+++ codigo_de_la_calculadora/task manager.py
@@ -0,0 +1,43 @@
+tasks = []
+
+def displaytasks(all_tasks):
+      print('|nYour tasks: ')
+      for index, task in enumerate(all_tasks):
+            print(f'{index + 1}. {task}')
+
+def newOperation(all_tasks):
+     opertaion = input('Press 'A' to add a task, 'D' to delete a task, 'V' to view all tasks 'h' to edit a tasks 'g'or the quit the application: ')
+  
+    if operation == 'A':
+    addTask(tasks)
+    
+    elif operation == 'D':
+        deleteTask(tasks)
+    
+    elif operation == 'V':
+        viewTasks(tasks)   
+     
+    elif operation == 'h':
+        editTask(tasks)
+    
+elif operation == 'g':
+        print('Good bye!')
+        exit()
+else:
+     newOperation(tasks)
+   
+
+def addTask(all_tasks):
+     new_task = input('add a task: ')
+     all_tasks.append(new_task)
+
+     for task in all_tasks:
+         print(task)
+
+displaytasks(tasks)
+
+newOperation(tasks)
+
+
+#start application
+addTask(tasks)
